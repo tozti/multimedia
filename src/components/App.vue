add2d4a127954dea845472dc83985d833b9c3725
gduboc <guillaume.duboc@ens-lyon.fr>
2018-03-01 00:01:17 +0100

    Extension loading is working

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,27 +2,17 @@
  <section class="fixed-content" style="padding: 20px">
         <div class="tc-content-wrapper">
             <div class="tc-content">
-                <nav class="level">
-                Hello
-              </nav>
+                HELLO
             </div>
         </div>
     </section>
 </template>
 
 <script>
-const SidebarStatus = Object.freeze({View: 0, Modify: 1, Create: 2})
-
 
   export default {
     data(){
         return{
-            sidebar : {
-                opened : false,
-                type : SidebarStatus.View,
-                event : {},
-                titles : ['Evenement', 'Modification', 'Ajout']
-            }
         }
     }
 }
